831702c471af5dbb88aee3c58ac6da8089aee2ba
Sirfvadukiya <sirfvadukiya36@gmail.com>
2025-10-02 21:59:37 +0530

    Add files via upload

---
+++ Sortt.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Sortt 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) 
+        {
+            a[i] = sc.nextInt();
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(a[i] != i + 1)
+            {
+                System.out.println("array is not sorted");
+                return;
+            }
+        }
+}
+}
